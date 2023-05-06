791edbbbebc9e682a836fe2c267095c8db079e67
John Patrick Valera <johnpatrickyusoresvalera@gmail.com>
2023-05-06 19:55:37 +0800

    added mocha-reporter for html reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,13 @@
 const { defineConfig } = require("cypress");
+const { defineConfig } = require("cypress")
 
 module.exports = defineConfig({
   defaultCommandTimeout: 8000,
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
